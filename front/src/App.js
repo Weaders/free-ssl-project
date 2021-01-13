0c7b02477175091e8eb77d05daef5831d41e75b4
ulweader <ulweader@gmail.com>
2021-01-13 22:13:25 +0300

    Add docker and header

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import './App.css';
+import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core'
 import Container from '@material-ui/core/Container'
 import { SSLForm } from './features/ssl-form/SSLForm'
 
+const useStyles = makeStyles({
+  headaerText: {
+    'font-style': 'italic',
+    'padding-right': '10px'
+  },
+  gitIcon: {
+    width: '32px'
+  },
+  gitIconContainer: {
+    position: 'absolute',
+    right: '10px'
+  }
+});
+
 function App() {
+
+  const styles = useStyles();
+
   return (
-    <Container component="main">
-        <SSLForm/>
-    </Container>
+    <React.Fragment>
+      <AppBar position="sticky" color="default" >
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap className={styles.headaerText}>
+            SSL Get
+          </Typography>
+          <div className={styles.gitIconContainer}>
+            <a href="https://github.com/Weaders/free-ssl-project" target="_blank"><img src="GitHub-Mark-120px-plus.png" className={styles.gitIcon} /></a>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Container component="main">
+        <SSLForm />
+      </Container>
+    </React.Fragment>
   );
 }
 
